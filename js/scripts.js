de7e48b4b1c0eafa362f0307c8c7f626a156cf7f
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-08 13:00:20 +0300

    Added the js from JSFiddle to a file in local directory

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var add = function(number1,number2){
+	return number1+number2;
+};
+var number1=parseInt(prompt("Enter the first number:"))
+var number2=parseInt(prompt("Enter the Second number:"))
+alert(add(number1,number2))
+
+var subtract=(number1, number2)=> number1-number2;
+alert(subtract(10,5))
