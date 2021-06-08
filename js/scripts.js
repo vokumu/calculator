233d5ad0cfcacd529cfe17b9566d4ab20a1285ca
victoria okumu <victoria.okumu@student.moringaschool.com>
2021-06-08 13:27:10 +0300

    Added the subtraction, multiplication and division functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
+//addition
 var add = function(number1,number2){
 	return number1+number2;
 };
-var number1=parseInt(prompt("Enter the first number:"))
-var number2=parseInt(prompt("Enter the Second number:"))
-alert(add(number1,number2))
-
-var subtract=(number1, number2)=> number1-number2;
-alert(subtract(10,5))
+//subtraction
+var subractNumbers=(number1,number2)=>number1-number2
+//multiplication
+var multiplyNumbers=(number1,number2)=>number1*number2
+//Division
+var divideNumbers=(number1,number2)=>number1/number2
+var number1=parseInt(prompt("Enter the first number:"));
+var number2=parseInt(prompt("Enter the Second number:"));
+var addResult=add(number1,number2)
+var subtractResult=subractNumbers(number1,number2)
+var multiplyResult=multiplyNumbers(number1,number2)
+var divideResult=divideNumbers(number1,number2)
+alert("Addition:"+addResult+"\nSubtraction:"+subtractResult+ "\nMultiplication:"+multiplyResult+"\nDivision:"+divideResult)
